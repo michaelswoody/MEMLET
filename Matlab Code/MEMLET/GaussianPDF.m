12f21903e2000bd1bd3c91e7dc36875d298b4778
Michael Woody <michael.s.woody@gmail.com>
2017-04-03 09:18:34 -0400

    v1.1 Added Camera PDF's (Lewis et al. 2017) and minor bug fixes Added the Camera discrete PDF's described by Lewis et al., Biophysical Jouranl 2017 Also added compatibility with Mac software and warning about needing optimization toolbox

--- Matlab Code/MEMLET/GaussianPDF.m
+++ Matlab Code/MEMLET/GaussianPDF.m
@@ -1,18 +1,21 @@
 function [probability] = GaussianPDF(x,mu,sig,varargin)
 
 %GaussianPDG custom PDF for single Gaussian distribution
+probability = exp(-((x-mu).^2)/(2.0*sig^2))/(sig*sqrt(2.0*pi));
 if nargin==4;
 	tmin=varargin{1};
     tmax=Inf;
+    probability=probability/(1-normcdf(tmin,mu,sig));   
 elseif nargin==5;
     tmin=varargin{1};
     tmax=varargin{2};
+    probability = exp(-((x-mu).^2)/(2.0*sig^2))/(sig*sqrt(2.0*pi));
+    probability=probability/(normcdf(tmax,mu,sig)-normcdf(tmin,mu,sig));
 else
-    tmin=0;
-    tmax=Inf;
+    probability = exp(-((x-mu).^2)/(2.0*sig^2))/(sig*sqrt(2.0*pi));
 end 
 
-probability = exp(-((x-mu).^2)/(2.0*sig^2))/(sig*sqrt(2.0*pi));
-probability=probability/(normcdf(tmax,mu,sig)-normcdf(tmin,mu,sig));
+
+
 end
 
