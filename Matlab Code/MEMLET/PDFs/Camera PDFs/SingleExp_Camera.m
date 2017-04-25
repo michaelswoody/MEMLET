665e3fcd81538e7ca6140f7bb4470c5963b3dd33
Michael Woody <michael.s.woody@gmail.com>
2017-04-25 15:58:39 -0400

    v 1.1.2 - IMPORTANT: Bug fixes for single and double Camera PDF files. Functionality was not as expected in release v1.1.1, so please update

--- Matlab Code/MEMLET/PDFs/Camera PDFs/SingleExp_Camera.m
+++ Matlab Code/MEMLET/PDFs/Camera PDFs/SingleExp_Camera.m
@@ -10,7 +10,7 @@ function [probability] = SingleExp_Camera(t,tau1,dt,tmin)
 phi=0.5; % threshold of intensity for camera data. Only is important for when fm=1.
 
 metaData=struct('name','Single Exp PDF for Camera',... %default values for MEMLET
-            'PDF',  'SingleExp_Camera(t,tau1,dt,tmin)',...
+            'PDF',  'SingleExp_Camera(t,tau1,del_t,tmin)',...
             'dataVar','t',...
             'fitVar', 'tau1',...
             'ub',   '10000',...
@@ -25,15 +25,15 @@ if nargin==1; % returns MetaData when called with only one argument
 end
 
 f=t./dt; %convert dwell times to number of frames
-
+fm=tmin/dt;
 if fm==1; %if fm=1 use the correction
     %equation has a factor of 1/dt to convert it from a probability mass
     %function to a probability density function for compatibility with
     %MEMLET's plotting and log-likelihood ratio test functions
       A=phi+(tau1/dt)*(1-exp(-((1-phi)*dt)/tau1)); %fm=1 correction factor Eqn S18
-      probability=(1-exp(-dt./tau1)).*exp(-(dt*(f-tmin/dt))./tau1)*1/A;
+      probability=(1/dt)*(1-exp(-dt./tau1)).*exp(-(dt*(f-tmin/dt))./tau1)*1/A;
 else %if fm>1 
-      probability=(1-exp(-dt./tau1)).*exp(-(dt*(f-tmin/dt))./tau1);
+      probability=(1/dt)*(1-exp(-dt./tau1)).*exp(-(dt*(f-tmin/dt))./tau1);
 end
 
 
